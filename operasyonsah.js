1bb839f945c27746fc16331b710a4b0d34be2dfd
Eksere <139393349+Eksere@users.noreply.github.com>
2023-08-18 18:26:37 +0300

    Update operasyonsah.js

--- operasyonsah.js
+++ operasyonsah.js
@@ -24,7 +24,7 @@ var ram = $('#unit_input_ram').next().html();
 var catapult = $('#unit_input_catapult').next().html();
 
 if (parseInt(ram.match(/\d+/g)) > 0) {
-    $('#unit_input_ram').val('1');
+    $('#unit_input_ram').val('scr5');
 } else if (parseInt(catapult.match(/\d+/g)) > 0) {
     $('#unit_input_catapult').val('1');
 }
