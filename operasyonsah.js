b21e2a0433d37c8130ff9734a5c5fe3dc5a38955
Eksere <139393349+Eksere@users.noreply.github.com>
2023-08-18 16:26:26 +0300

    Update operasyonsah.js

--- operasyonsah.js
+++ operasyonsah.js
@@ -1,27 +1,32 @@
-Kons = KNS.split(' ');
-i = 0;
+var KNS = "your_space_delimited_values_here";
+var Kons = KNS.split(' ');
+var i = 0;
+
 if ($.cookie('Konsantre') != null) {
-    i = $.cookie("Konsantre");
+    i = parseInt($.cookie('Konsantre'));
 }
+
 if (i >= Kons.length) {
     i = 0;
 }
-Koor = Kons[i];
-Koor = Koor.split('|');
+
+var Koor = Kons[i].split('|');
 i++;
 
 document.forms[0].x.value = Koor[0];
 document.forms[0].y.value = Koor[1];
+
 var date = new Date();
 date.setTime(date.getTime() + (10 * 60 * 1000));
-$.cookie("Konsantre", i, { expires: date });
+$.cookie('Konsantre', i.toString(), { expires: date });
 
 var ram = $('#unit_input_ram').next().html();
 var catapult = $('#unit_input_catapult').next().html();
 
-if (ram.match(/\d+/g) > 0) {
+if (parseInt(ram.match(/\d+/g)) > 0) {
     $('#unit_input_ram').val('1');
-} else if (catapult.match(/\d+/g) > 0) {
+} else if (parseInt(catapult.match(/\d+/g)) > 0) {
     $('#unit_input_catapult').val('1');
 }
+
 end();
