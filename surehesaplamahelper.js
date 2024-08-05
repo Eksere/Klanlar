9f9e6a9a5ed3ec1222f217772963075d65081e2a
Eksere <139393349+Eksere@users.noreply.github.com>
2024-08-05 17:49:33 +0300

    Update surehesaplamahelper.js

--- surehesaplamahelper.js
+++ surehesaplamahelper.js
@@ -105,9 +105,7 @@ try {
 		var twcode = `[size=12][b]Saldırı Zamanı: ${land_time}[/b][/size][table][*]\n\n`;
     
 		var colour = '';
-
-		var colour = '';
-
+		
 		for (attack in plan) {
 			if (
 				plan[attack]['target'] != undefined ||
@@ -126,6 +124,17 @@ try {
 
 				var formattedDate = launch_time.toString();
 				formattedDate = formatDateTime(formattedDate);
+				commands.forEach((command) => {
+                const { id, fromCoord, toCoord, formattedLaunchTime, unit } =
+                    command;
+                const [toX, toY] = toCoord.split('|');
+
+                let sitterId =
+                    game_data.sitter > 0 ? `t=${game_data.player.id}` : '';
+                let fillRallyPoint =
+                    game_data.market !== 'uk'	
+				? `&x=${toX}&y=${toY}${SEND_UNITS}`
+                      		   : '';		
 				let commandUrl = `/game.php?${sitterId}&village=${id}&screen=place${fillRallyPoint}`;
 				twcode +=
 					get_troop(plan[attack]['type']) +
