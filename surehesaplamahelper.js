7571c5cbe2c9605ef937920ab848d2281fd1f158
Eksere <139393349+Eksere@users.noreply.github.com>
2024-08-05 18:07:15 +0300

    Update surehesaplamahelper.js

--- surehesaplamahelper.js
+++ surehesaplamahelper.js
@@ -125,7 +125,7 @@ try {
 				var formattedDate = launch_time.toString();
 				formattedDate = formatDateTime(formattedDate);
 						
-				let commandUrl = `/game.php?${sitterId}&village=${id}&screen=place${fillRallyPoint}`;
+				var commandUrl = `/game.php?${sitterId}&village=${id}&screen=place${fillRallyPoint}`;
 				twcode +=
 					get_troop(plan[attack]['type']) +
 					' [|] ' +
