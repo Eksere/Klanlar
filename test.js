fecf3d20b7f7548ac28732eaef18b4c1ffce9d5f
Eksere <139393349+Eksere@users.noreply.github.com>
2024-08-05 13:13:07 +0300

    Update test.js

--- test.js
+++ test.js
@@ -155,7 +155,10 @@ function init(unitInfo) {
 
 // Helper: Window Content
 function prepareWindowContent(windowBody) {
-    const windowHeader = `<h1 class="ra-fs18 ra-fw600">${scriptData.name}</h6>`;
+let bbCode = `[table]`;
+    const windowHeader bbCode += `<h1 class="ra-fs18 ra-fw600">${scriptData.name}</h1>`;bbCode += `[/table]`;
+
+            return bbCode;
     const windowFooter = `<small><strong>${scriptData.name} ${scriptData.version}</strong> - <a href="${scriptData.authorUrl}" target="_blank" rel="noreferrer noopener">${scriptData.author}</a> - <a href="${scriptData.helpLink}" target="_blank" rel="noreferrer noopener">Help</a></small>`;
     const windowStyle = `
 		<style>
