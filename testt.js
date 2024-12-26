511e3db02f5c5c67c04366c2b9048778303d5613
Eksere <139393349+Eksere@users.noreply.github.com>
2024-12-27 00:20:18 +0300

    Update testt.js

--- testt.js
+++ testt.js
@@ -1,3 +1,143 @@
+if (window.location.href.indexOf('premium&mode=log&page=') < 0) {
+    //relocate
+    window.location.assign(game_data.link_base_pure + "premium&mode=log&page=0");
+}
+
+if (localStorage.getItem("PPLogShinko")) {
+    temp = JSON.parse(localStorage.getItem("PPLogShinko"));
+    console.log("Loading previous data");
+    stopDate = temp.lastDate;
+    stopChange = temp.lastChange;
+    var farmed = temp.farmed;
+    var spending = temp.spending;
+    var totalFarmed = temp.totalFarmed;
+    var totalSpent = temp.totalSpent;
+} else {
+    stopDate = 0;
+    stopChange = 0;
+    var farmed = [];
+    var spending = [];
+    var totalFarmed = 0;
+    var totalSpent = 0;
+}
+
+if (game_data.player.sitter > 0) {
+    baseURL = `/game.php?t=${game_data.player.id}&screen=premium&mode=log&page=`;
+} else {
+    baseURL = "/game.php?&screen=premium&mode=log&page=";
+}
+
+var amountOfPages = parseInt(
+    $(".paged-nav-item")[$(".paged-nav-item").length - 1].href.match(/page=(\d+)/)[1]
+);
+var URLs = [];
+for (var i = 0; i <= amountOfPages; i++) {
+    URLs.push(baseURL + i);
+}
+
+$.getAll = function (
+    urls, // array of URLs
+    onLoad, // called when any URL is loaded, params (index, data)
+    onDone, // called when all URLs successfully loaded, no params
+    onError // called when a URL load fails or if onLoad throws an exception, params (error)
+) {
+    var numDone = 0;
+    var lastRequestTime = 0;
+    var minWaitTime = 200; // ms between requests
+    loadNext();
+    function loadNext() {
+        if (numDone == urls.length) {
+            onDone();
+            return;
+        }
+
+        let now = Date.now();
+        let timeElapsed = now - lastRequestTime;
+        if (timeElapsed < minWaitTime) {
+            let timeRemaining = minWaitTime - timeElapsed;
+            setTimeout(loadNext, timeRemaining);
+            return;
+        }
+
+        lastRequestTime = now;
+        $.get(urls[numDone])
+            .done((data) => {
+                try {
+                    onLoad(numDone, data);
+                    ++numDone;
+                    loadNext();
+                } catch (e) {
+                    onError(e);
+                }
+            })
+            .fail((xhr) => {
+                onError(xhr);
+            });
+    }
+};
+
+$.getAll(
+    URLs,
+    (i, data) => {
+        console.log("Grabbing page " + i);
+        let tempRows = $(data).find("table .vis> tbody > tr");
+        for (let j = 2; j < tempRows.length; j++) {
+            let transactionType = tempRows[j].children[2].innerText.trim();
+            let amount = parseInt(tempRows[j].children[3].innerText.trim());
+
+            if (transactionType.indexOf("Transfer") > -1 &&
+                (tempRows[j].children[5].innerText.indexOf("Satıldı") > -1 ||
+                 tempRows[j].children[5].innerText.indexOf("Premium Takası") > -1)) {
+                farmed.push({
+                    Date: tempRows[j].children[0].innerText,
+                    Amount: amount
+                });
+                totalFarmed += amount;
+            }
+
+            if (transactionType.indexOf("Premium Takası") > -1) {
+                spending.push({
+                    Date: tempRows[j].children[0].innerText,
+                    Amount: amount
+                });
+                totalSpent += amount;
+            }
+        }
+    },
+    () => {
+        console.log("Total farmed: " + totalFarmed);
+        console.log("Total spent: " + totalSpent);
+
+        let html = `
+            <div>
+                <h2>Kasılan Premium Puanlar</h2>
+                <table class="vis" width="100%">
+                    <tr>
+                        <th>Tarih</th>
+                        <th>Miktar</th>
+                    </tr>
+                    ${farmed.map(f => `<tr><td>${f.Date}</td><td>${f.Amount}</td></tr>`).join('')}
+                </table>
+                <h3>Toplam: ${totalFarmed} PP</h3>
+
+                <h2>Harcanan Premium Puanlar</h2>
+                <table class="vis" width="100%">
+                    <tr>
+                        <th>Tarih</th>
+                        <th>Miktar</th>
+                    </tr>
+                    ${spending.map(s => `<tr><td>${s.Date}</td><td>${s.Amount}</td></tr>`).join('')}
+                </table>
+                <h3>Toplam: ${totalSpent} PP</h3>
+            </div>`;
+
+        $("#contentContainer").html(html);
+    },
+    (error) => {
+        console.error(error);
+    }
+);
+
 
 if (window.location.href.indexOf('premium&mode=log&page=') < 0) {
     //relocate
